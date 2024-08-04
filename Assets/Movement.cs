4bbdd4793ce9662fe6d514d6b5419812fa586e7d
Vis <Vish106@rambler.ru>
2024-08-04 08:09:12 +0300

    2

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private float _rotationSpeed;
-    [SerializeField] private float _scale;
 
     private void Update()
     {
-        transform.Translate(_speed * Time.deltaTime * Vector3.left);
-        transform.Rotate(_rotationSpeed * Time.deltaTime * Vector3.up);
-        transform.localScale += new Vector3(1, 1, 1) * _scale * Time.deltaTime;
+        transform.Translate(_speed * Time.deltaTime * Vector3.left);       
     }
-
 }
