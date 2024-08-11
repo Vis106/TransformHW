e410ecf6833e949fdea5e486ffe4847c0a46af60
Vis <Vish106@rambler.ru>
2024-08-11 04:31:08 +0300

    attemp #2

--- Assets/Counter.cs
+++ Assets/Counter.cs
@@ -1,37 +1,53 @@
+using System;
 using System.Collections;
-using TMPro;
 using UnityEngine;
 
 public class Counter : MonoBehaviour
-{
-
-    [SerializeField] private TextMeshProUGUI _countText;
+{   
     [SerializeField] private float _countStepTime = 0.5f;
     [SerializeField] private float _countStep = 1f;
 
     private float _count = 0f;
     private bool _isCounting = false;
     private WaitForSeconds _wait;
+    private Coroutine _countCoroutine;
+    private byte _mousebutton = 0;
+
+    public static Action<float> Changed;
 
     private void Awake()
     {
+        _countCoroutine = StartCoroutine(CountingBySteps());
         _wait = new WaitForSeconds(_countStepTime);
     }
 
-    void Update()
+    private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(_mousebutton))
         {
             if (_isCounting == false)
             {
                 _isCounting = true;
-                StartCoroutine(CountingBySteps());
+                Start();
             }
             else
             {
                 _isCounting = false;
-                StopCoroutine(CountingBySteps());
+                Stop();
             }
+        }
+    }
+
+    public void Start()
+    {
+        _countCoroutine = StartCoroutine(CountingBySteps());
+    }
+
+    public void Stop()
+    {
+        if (_countCoroutine != null)
+        {
+            StopCoroutine(_countCoroutine);
         }
     }
 
@@ -40,7 +56,7 @@ public class Counter : MonoBehaviour
         while (_isCounting)
         {
             _count += _countStep;
-            _countText.text = _count.ToString();
+            Changed?.Invoke(_count);
             yield return _wait;
         }
     }
