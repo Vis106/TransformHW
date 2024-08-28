87d661e3629f56f66159b51c9873c1cf85fd2c46
Vis <Vish106@rambler.ru>
2024-08-28 03:01:57 +0300

    attempt 2

--- Assets/Scripts/Creator.cs
+++ Assets/Scripts/Creator.cs
@@ -6,51 +6,45 @@ public class Creator : MonoBehaviour
     [SerializeField] private Cube _cubePrefab;
     [SerializeField] private float _explotionForce;
     [SerializeField] private float _explotionRadius;
-    [SerializeField] private float _decreaseScaleRate = 0.5f;
-
+   
     private int _maxCubeInstanstiate = 6;
     private int _minCubeInstanstiate = 2;
-    private int _positionGap = 2;
     private int _maxChance = 100;
-    private int _minChance = 1;
+    private int _minChance = 1;      
 
-    private void OnMouseUpAsButton()
+    public void Divide()
     {
         Explode();
         Destroy(gameObject);
-        CreateCube();
+        CreateCubes();       
     }
 
     private void Explode()
     {
-        foreach (Cube explodableObject in CreateCube())
+        foreach (Cube explodableObject in CreateCubes())
         {
             explodableObject.TryGetComponent<Rigidbody>(out Rigidbody explodableCube);
-            explodableCube.AddExplosionForce(_explotionForce, transform.position, _explotionRadius);
+            explodableCube?.AddExplosionForce(_explotionForce, transform.position, _explotionRadius);
         }
     }
 
-    private bool CheckChance()
+    private bool CanCreate()
     {
         var currentChance = Random.Range(_minChance, _maxChance);
         return currentChance <= _cubePrefab.ChanceCreateCube;
     }
 
-    private List<Cube> CreateCube()
+    private List<Cube> CreateCubes()
     {
         List<Cube> cubes = new();
         int cubeCount = Random.Range(_minCubeInstanstiate, _maxCubeInstanstiate);
 
-        if (CheckChance())
+        if (CanCreate())
         {
             for (int i = 0; i < cubeCount; i++)
             {
                 var cube = Instantiate(_cubePrefab, transform.position, transform.rotation);
-                int positionGap = Random.Range(0, _positionGap);
-                cube.transform.position += new Vector3(positionGap, positionGap, positionGap);
-                cube.ChangeChance();
-                cube.ChangeColor();
-                cube.ChangeScale();
+                cube.Init();               
             }
         }
 
--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,28 +1,27 @@
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Renderer))]
 public class Cube : MonoBehaviour
 {
     [SerializeField] private float _decreaseChanceRate = 0.5f;
     [SerializeField] private float _decreaseScaleRate = 0.5f;
-    [SerializeField] private float _chanceCreateCube = 100;
-
+    [SerializeField] private float _chanceCreateCube = 100;  
+    [SerializeField] private Creator _creator;
+     
     public float ChanceCreateCube => _chanceCreateCube;
-    public float DecreaseRate => _decreaseChanceRate;
+    public float DecreaseRate => _decreaseChanceRate;    
 
-    public void ChangeChance()
+    private void OnMouseUpAsButton()
+    {
+        _creator.Divide();          
+    }
+
+    public void Init()
     {
         _chanceCreateCube *= _decreaseChanceRate;
-    }
-
-    public void ChangeScale()
-    {
-        Vector3 scaleChange = transform.localScale / _decreaseScaleRate;       
+        Vector3 scaleChange = transform.localScale / _decreaseScaleRate;
         transform.localScale -= scaleChange;
-    }
-
-    public void ChangeColor()
-    {
-        GetComponent<Renderer>().material.color = Random.ColorHSV();            
+        GetComponent<Renderer>().material.color = Random.ColorHSV();
     }
 }
